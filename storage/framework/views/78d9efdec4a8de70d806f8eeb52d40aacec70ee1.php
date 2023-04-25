191cd5e02ff93c2b5aa49c7eeca8f48fa16d6f13
NgHung111 <thienhungnguyen397@gmail.com>
2023-04-25 10:59:21 +0700

    insertproduct

---
+++ storage/framework/views/78d9efdec4a8de70d806f8eeb52d40aacec70ee1.php
@@ -0,0 +1,8 @@
+<meta charset="utf-8">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<link rel="icon" type="image/x-icon" href="<?php echo e(asset('assets/admin/images/logo.ico')); ?>">
+<title>HL Shoe</title>
+
+<?php /**PATH C:\Users\HUNG\Desktop\DoAn\resources\views/admin/layouts/head.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/b270e0f4e4078a74671fe140fa9a7d9144a0286a.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php echo $__env->make('admin.layouts.head', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+    <?php echo $__env->make('admin.layouts.css', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+    
+</head>
+
+<body>
+    <!-- Preloader -->
+    
+    <div class="contain_main wrapper">
+        <?php echo $__env->make('admin.layouts.menu', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+        <?php echo $__env->yieldContent('body'); ?>
+        <?php echo $__env->make('admin.layouts.footer', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+    </div>
+    <?php echo $__env->make('admin.layouts.js', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+</body>
+
+</html>
+<?php /**PATH C:\Users\HUNG\Desktop\DoAn\resources\views/admin/index.blade.php ENDPATH**/ ?>
